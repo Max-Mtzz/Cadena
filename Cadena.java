db41fce59f422c1350fe76362fb433a4f2df6c28
Max-Mtzz <20243ds119@utez.edu.mx>
2025-05-08 09:34:07 -0600

    Ya acabe

--- Cadena.java
+++ Cadena.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class Cadena{
     public static void main(String[] args){
         String nombre;
+        String apellido;
         Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Escribe tu Nombre: ");
+        String nombre = entrada.nextLine();
+        System.out.println("Escribe tu Apellido: ");
+        String nombre = entrada.nextLine();
+        System.out.println("Tu nombre es: "+ nombre +" "+ apellido);
     }
 }
